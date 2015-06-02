d4e3cac8d187946d2895e6192d25422b987853eb
hgurmen <hgurmen@gmail.com>
2015-06-02 14:34:11 -0300

    Limpié un poco el código y hecho hasta reduce de listas

--- ArrSeq.hs
+++ ArrSeq.hs
@@ -13,104 +13,34 @@ isEven :: Integral a => a -> Bool
 isEven x
     | mod x 2 == 0 = True
     | otherwise = False
+    
 
 singletonA :: a -> Arr a
 singletonA x = Arr.fromList [x]
 
--- Preguntar: cual es más efectivo??
+
 appendA :: Arr a -> Arr a -> Arr a
 appendA s s' = flatten (Arr.fromList [s, s'])
 
--- La función que aplico a tabulate es O(1) luego la profundidad
--- es O(1) y el trabajo es O(|s| + |s'|) pues recorro todo
-_appendA :: Arr a -> Arr a -> Arr a
-_appendA s s' = tabulate (\i -> if i < ls then s!i else s'!(i - ls)) total
-    where ls = Arr.length s
-          ls' = Arr.length s'
-          total = ls + ls'
           
 takeA :: Arr a -> Int -> Arr a
 takeA s n = subArray 0 n s
 
+
 dropA :: Arr a -> Int -> Arr a
 dropA s n = subArray n (len - n) s
     where len = Arr.length s
+
 
 mapA :: (a -> b) -> Arr a -> Arr b
 mapA f s = tabulate (\i -> f (s!i)) len
     where len = Arr.length s
 
 
--- filterA no tiene buen costo ya que usa append y en el caso de trabajo,
--- append tiene trabajo O(|s| + |s'|) y eso implica que quede trabajo cuadrático
--- SOLO en el append del final...
 filterA :: (a -> Bool) -> Arr a -> Arr a
-filterA f s
-    | len == 0 = empty
-    | len == 1 = if f (s!0) then singletonA (s!0)
-                 else empty
-    | otherwise = let (l, r) = filterA f (takeA s m)
-                               |||
-                               filterA f (dropA s m)
-                  in appendA l r
-    where len = Arr.length s
-          m = div len 2
-          
--- Este rinde mas, hace un flatten que no tengo ni idea cual es el costo (aparentemente
--- tiene W O(|s|) y S O(lg |s|) donde s es la secuencia de secuencias.
-fltrA :: (a -> Bool) -> Arr a -> Arr a
-fltrA f s = flatten (tabulate (\i -> if f (s!i) then singletonA (s!i) else empty) len)
-    where len = Arr.length s
-    
--- PREGUNTAR: join se tiene que implementar con flatten o hay que hacerla nosotros?
--- una idea de hacer join es usando tabulate y reduce, capaz
--- PREGUNTAR: aparentemente queda cuadrático
-{-
-ejemplo:
-tengo
-<a1,a2,a3,a4>
-con longitudes a,b,c,d respectivamente
-
-reduce que hace primero:
-append de a1 + a2 => O(a + b)
-append de a3 + a4 => O(c + d)
-append de (a1 + a2) + (a3 + a4) = O(a + b + c + d)
--}
-joinA :: Arr (Arr a) -> Arr a
-joinA s = reduceA fun empty s
-    where fun = \a -> \b -> appendA a b
-    
-test1 = Arr.fromList [1,2,3,4]
-test2 = Arr.fromList [5,6,7]
-test3 = Arr.fromList [8,9]
-test4 = empty :: Arr Int
-test5 = Arr.fromList [10,11,12]
-test6 = empty :: Arr Int
-test7 = singletonA 13
-test = Arr.fromList [test1,test2,test3,test4,test5,test6,test7]
-
-{-
--- Integer Logarithm base 2
-ilg :: Int -> Int
-ilg 1 = 0
-ilg 2 = 1
-ilg x = 1 + ilg (div x 2)
-
-rdc :: (a -> a -> a) -> Arr a -> a
-rdc f s
-    | len == 1 = s!0
-    | otherwise = let m = 2^(ilg (len - 1))
-                      (l, r) = rdc f (takeA s m)
-                               |||
-                               rdc f (dropA s m)
-                  in f l r
+filterA f s = flatten (tabulate (\i -> if f (s!i) then singletonA (s!i) else empty) len)
     where len = Arr.length s
 
-reduceA :: (a -> a -> a) -> a -> Arr a -> a
-reduceA f b s
-    | Arr.length s == 0 = b
-    | otherwise = f b (rdc f s)
--}
   
 showtA :: Arr a -> TreeView a (Arr a)
 showtA s 
@@ -119,6 +49,7 @@ showtA s
     | otherwise = NODE (takeA s m) (dropA s m)
     where len = Arr.length s
           m = div len 2
+
 
 showlA :: Arr a -> ListView a (Arr a)
 showlA s
@@ -145,6 +76,7 @@ contract f s
           odd_tab i = if 2*i == (len - 1) then s!(2*i) -- O(W/S f)
                       else f (s!(2*i)) (s!(2*i + 1))
 
+
 reduceA :: (a -> a -> a) -> a -> Arr a -> a
 reduceA f b s
     | len == 0 = b
@@ -152,16 +84,14 @@ reduceA f b s
     | otherwise = reduceA f b (contract f s)
     where len = Arr.length s
 
--- Nota: esto hace majia pura papá, este tp es usar tabulate siempre que puedas.
--- Hace lo que dice en los apuntes, básicamente.
--- PREGUNTAR: así es como debe hacerse?
+
 combine :: (a -> a -> a) -> Arr a -> Arr a -> Arr a
---combine f xs s' = Arr.tabulate (\i -> if isEven i then s'!(div i 2) else f (s'!(div i 2)) (xs!(i-1))) len
 combine f s s' = tabulate fun len
     where len = Arr.length s
           fun i = let m = div i 2
                   in if isEven i then s'!m -- O(1) s'!(i/2)
                      else f (s'!m) (s!(i - 1)) -- O(W/S f) s'!(i/2) `f` s!(i -1)
+
 
 scanA :: (a -> a -> a) -> a -> Arr a -> (Arr a, a)
 scanA f b s
@@ -171,7 +101,7 @@ scanA f b s
                   in (combine f s s', rdc)
     where len = Arr.length s
 
--- Preguntar:
+
 instance Seq Arr where
     emptyS = empty
     singletonS = singletonA
--- ListSeq.hs
+++ ListSeq.hs
@@ -12,11 +12,14 @@ tabulateL :: (Int -> a) -> Int -> [a]
 tabulateL f 0 = []
 tabulateL f n = (f 0) : (tabulateL (f . (+1)) (n - 1))
 
+
 takeL :: [a] -> Int -> [a]
 takeL xs n = Prelude.take n xs
 
+
 dropL :: [a] -> Int -> [a]
 dropL xs n = Prelude.drop n xs
+
 
 filterL :: (a -> Bool) -> [a] -> [a]
 filterL f [] = []
@@ -24,66 +27,39 @@ filterL f (x:xs) = let (hd, tl) = f x ||| filterL f tl
                    in if hd then x : filterL f tl
                       else filterL f tl
 
+
 mapL :: (a -> b) -> [a] -> [b]
 mapL f [] = []
-mapL f (x:xs) = let (hd, tl) = fx ||| mapL f tl
+mapL f (x:xs) = let (hd, tl) = f x ||| mapL f tl
                 in hd : tl
 
--- PREGUNTAR: paralelizar el take y el drop en este caso no hace niguna mejora
--- con respecto al trabajo y/o profundidad, verdad?
--- Simplemente llevaria de O(n) a O(n/2) que es lo mismo...
+
 showtL :: [a] -> TreeView a [a]
 showtL []  = EMPTY
 showtL [x] = ELT x
-showtL s  = let len = Prelude.length s
-                m = div len 2
-                (l, r) = takeL s m ||| dropL s m
-            in NODE l r
+showtL s = let len = Prelude.length s
+               m = div len 2
+               (l, r) = splitAt m s
+           in NODE l r
 
--- W = 1, S = 1
+
 showlL :: [a] -> ListView a [a]
 showlL []     = NIL
 showlL (x:xs) = CONS x xs
 
--- PREGUNTAR: Hay que paralelizar el cálculo de f x y junto con el del paso recursive de reduceStep????
--- Aún cuando paralelizás esto tendría costo lineal, no?
+
 contract :: (a -> a -> a) -> [a] -> [a]
-contract f []       = []
-contract f [x]      = [x]
-contract f (x:y:xs) = f x y : contract f xs
---contract f (x:y:xs) = let (fun, resto) = f x y ||| contract f xs
---                      in fun : resto
+contract f [] = []
+contract f [x] = [x]
+contract f (x:y:xs) = let (hd, tl) = f x y ||| contract f xs
+                      in hd : tl
 
-{-
-versión paralela de contract
-El trabajo de cntrct es sum de i=0 a |xs|-1 de W(f x y)
-
-La profundidad sería:
-|xs| + máximo de todas las aplicaciones en el árbol de reducción de S(f x y)
--}
-
-cntrct :: (a -> a -> a) -> [a] -> [a]
-cntrct f [] = []
-cntrct f [x] = [x]
-cntrct f (x:y:xs) = let (hd, tl) = f x y ||| cntrct f xs
-                    in hd : tl
-
--- W = n + (sumatoria de 0 a log2 n f)
-{-
-reduceL se va aplicando recursivamente con una secuencia con la mitad de
-tamaño hasta llegar a lista de 1 elemento (logarítmico)
-
--}
 
 reduceL :: (a -> a -> a) -> a -> [a] -> a
 reduceL f b [] = b
 reduceL f b [x] = f b x
 reduceL f b s = reduceL f b (contract f s)
 
-{- --para debuggear
-let asd = contract f xs
-                  in trace (show xs ++ " -> " ++ show asd) reduceS f b asd
--}
 
 scanL :: (a -> a -> a) -> a -> [a] -> ([a], a)
 scanL f b []  = ([], b) -- da como resultado ([], b)
@@ -123,11 +99,9 @@ Funciona:
     Es orden lineal en la longitud de la primer lista (la original)
 -}
 
--- combine f s s' IndiceActualEsPar   ----->> algo
+-- combine f s s' IndiceActualEsPar   ----->> 
 combine :: (a -> a -> a) -> [a] -> [a] -> Bool -> [a]
-combine _ []       _           _ = []
--- PREGUNTAR: esto creo que no hace falta...
---combine _ _        []          _ = []
+combine _ []       _           _     = []
 combine f s@(x:xs) s'@(x':xs') True  = x' : (combine f s s' False) -- O(1)
 combine f [x]      (x':xs')    False = []
 combine f (x:_:xs) (x':xs')    False = let (hd, tl) = (f x' x) ||| (combine f xs xs' True) -- O(f)
